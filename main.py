390b12740ee6191320e1d5b332727fd11fc313cb
ozcakib <berkan.ozcakir@hva.nl>
2021-06-24 16:25:01 +0200

    style: Add comments

--- main.py
+++ main.py
@@ -26,17 +26,21 @@ def get_latest_news_articles():
 
 # Function to start fetching news_articles and add a tweet on twitter with fetched data
 def run():
+    # Fetch latest news articles
     found_articles = get_latest_news_articles()
 
+    # Initialize twitter Api
     auth = tweepy.OAuthHandler(config.twitter_consumer_key, config.twitter_consumer_secret_key)
     auth.set_access_token(config.twitter_access_token, config.twitter_access_token_secret)
-
     api = tweepy.API(auth)
 
+    # Loop over all the found articles
     for article in found_articles:
+        # Try to post a tweet with in the status the article
         try:
             api.update_status(article)
         except:
+            # If the post is identical to another post show this with a message and go on to the next article
             print("Already posted")
 
 
